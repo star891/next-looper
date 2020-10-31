8d0b60bcd07123da98d954b537a8a657b84fe04b
Simsek Mert <simsek.mert@cbsinteractive.com>
2020-10-31 11:12:41 -0400

    homepage and bootstrap stiling

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fcafc354c0796d424615";
+/******/ 	var hotCurrentHash = "bf75928f72ac155e2144";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
