cf683c5bdca1a82062e49fbd95e2a6ec9d06a390
Simsek Mert <simsek97@gmail.com>
2020-10-31 22:20:47 -0400

    cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,21 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/prop-types */
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache(),
+});
+
 function NextLooper({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default NextLooper;
